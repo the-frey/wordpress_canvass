00a5bb4b785e034bc33b7caccd66568056d3987a
Alex Lynham <acelynham@hotmail.com>
2012-11-14 18:50:22 +0000

    Should be all there

---
+++ header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+	<?php if (is_search()) { ?>
+	   <meta name="robots" content="noindex, nofollow" /> 
+	<?php } ?>
+
+	<title>
+		   <?php
+		      if (function_exists('is_tag') && is_tag()) {
+		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+		      elseif (is_archive()) {
+		         wp_title(''); echo ' Archive - '; }
+		      elseif (is_search()) {
+		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		         wp_title(''); echo ' - '; }
+		      elseif (is_404()) {
+		         echo 'Not Found - '; }
+		      if (is_home()) {
+		         bloginfo('name'); echo ' - '; bloginfo('description'); }
+		      else {
+		          bloginfo('name'); }
+		      if ($paged>1) {
+		         echo ' - page '. $paged; }
+		   ?>
+	</title>
+	
+	<link rel="shortcut icon" href="/favicon.ico">
+	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+	<?php if ( is_singular() ) wp_enqueue_script('comment-reply'); ?>
+
+	<?php wp_head(); ?>
+
+
+    <!--Here's jQuery -->
+    <script type="text/javascript" src="js/jquery-1.8.2.js"></script>
+
+    <!--And here our erstwhile hero uses jQuery to wow and delight! -->
+
+	  <script type="text/javascript">
+	  </script>
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+</head>
+
+<body <?php body_class(); ?>>
+	
+	<div id="page-wrap">
+
+		<div id="header">
+			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+			<div class="description"><?php bloginfo('description'); ?></div>
+		</div>
---
+++ home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta description="" />
+
+    <title></title>
+
+ 	<link rel="shortcut icon" href="images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <!--Here's jQuery -->
+    <script type="text/javascript" src="js/jquery-1.8.2.js"></script>
+
+    <!--And here our erstwhile hero uses jQuery to wow and delight! -->
+
+	  <script type="text/javascript">
+	  </script>
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->	    
+
+</head>
+<body>
+<div id="wrapper">
+	<div id="insideWrapper">
+		
+		<nav id="navContainer">
+			<ul id="mainNav">
+				
+			</ul>
+		</nav>
+
+		<div class="content">
+			
+		</div>
+	</div>
+
+	<footer>
+	</footer>
+	
+</div>
+</body>
+</html>
